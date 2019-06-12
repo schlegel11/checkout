89d4193cf9a4bbb2d58a9026f845ea137412f3e1
Marcel Schlegel <marcel@schlegel11.de>
2019-06-12 16:31:43 +0200

    Add unit test project.

---
+++ SupermarketCheckout/TestSupermarketCheckout/DateTimeRange.cs
@@ -0,0 +1,47 @@
+using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class DateTimeRange
+    {
+        [Test]
+        public void TestConstructionPositive()
+        {
+            var startDate = DateTime.Now.AddDays(-1);
+            var endDate = DateTime.Now.AddDays(1);
+            var dateTimeRange = new SupermarketCheckout.Utils.DateTimeRange(startDate, endDate);
+
+            Assert.AreEqual(dateTimeRange.Start, startDate);
+            Assert.AreEqual(dateTimeRange.End, endDate);
+        }
+
+        [Test]
+        public void TestConstructionNegative()
+        {
+            var startDate = DateTime.Now.AddDays(-1);
+            var endDate = DateTime.Now.AddDays(1);
+
+            Assert.Throws(typeof(ArgumentException),
+                () => new SupermarketCheckout.Utils.DateTimeRange(endDate, startDate),
+                "Start date should be <= end date.");
+        }
+
+        [Test]
+        public void TestIsInRangePositive()
+        {
+            var dateTimeRange =
+                new SupermarketCheckout.Utils.DateTimeRange(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
+            Assert.True(dateTimeRange.IsInRange(DateTime.Now));
+        }
+
+        [Test]
+        public void TestIsInRangeNegative()
+        {
+            var dateTimeRange =
+                new SupermarketCheckout.Utils.DateTimeRange(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
+            Assert.False(dateTimeRange.IsInRange(DateTime.Now.AddDays(-2)));
+            Assert.False(dateTimeRange.IsInRange(DateTime.Now.AddDays(2)));
+        }
+    }
+}
