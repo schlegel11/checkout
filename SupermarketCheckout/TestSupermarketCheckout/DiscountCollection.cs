6e716f4a5255b88afe39d6b1502e0629b54bb8e1
Marcel Schlegel <marcel@schlegel11.de>
2019-06-12 17:41:43 +0200

    Add DiscountCollection Test.

---
+++ SupermarketCheckout/TestSupermarketCheckout/DiscountCollection.cs
@@ -0,0 +1,66 @@
+using System;
+using NUnit.Framework;
+using SupermarketCheckout;
+
+namespace TestSupermarketCheckout
+{
+    public class DiscountCollection
+    {
+        private SupermarketCheckout.DiscountCollection discountCollection;
+
+        [SetUp]
+        public void Setup()
+        {
+            discountCollection = new SupermarketCheckout.DiscountCollection();
+            discountCollection.ValidityRange =
+                new SupermarketCheckout.Utils.DateTimeRange(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
+        }
+
+        [Test]
+        public void TestAddPositive()
+        {
+            var item = new Item();
+            var discount = new Discount();
+            discountCollection.Add(item, discount);
+            Assert.AreEqual(discountCollection.GetOrDefault(item, null), discount);
+        }
+
+        [Test]
+        public void TestAddNegative()
+        {
+            var item = new Item();
+            var discount = new Discount();
+            Assert.Throws(typeof(ArgumentNullException), () => discountCollection.Add(null, discount));
+            Assert.Throws(typeof(ArgumentNullException), () => discountCollection.Add(item, null));
+        }
+
+        [Test]
+        public void TestGetOrDefaultPositive()
+        {
+            var item = new Item();
+            var discount = new Discount();
+            discountCollection.Add(item, discount);
+            Assert.AreEqual(discountCollection.GetOrDefault(item, null), discount);
+            Assert.Null(discountCollection.GetOrDefault(new Item{Name = "OtherName"}, null));
+        }
+
+        [Test]
+        public void TestGetOrDefaultNegative()
+        {
+            Assert.Throws(typeof(ArgumentNullException), () => discountCollection.GetOrDefault(null, new Discount()));
+        }
+
+        [Test]
+        public void TestIsValidPositive()
+        {
+            Assert.True(discountCollection.IsValid(DateTime.Now));
+        }
+
+        [Test]
+        public void TestIsValidNegative()
+        {
+            Assert.False(discountCollection.IsValid(DateTime.Now.AddDays(-2)));
+            Assert.False(discountCollection.IsValid(DateTime.Now.AddDays(2)));
+        }
+    }
+}
