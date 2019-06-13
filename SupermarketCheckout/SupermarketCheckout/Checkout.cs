d3f1b2372c35a9478ecd8fc7564fc43b16af626f
Marcel Schlegel <marcel@schlegel11.de>
2019-06-13 11:49:56 +0200

    Made NoDiscount constant public. Add comments. Code cleanup.

--- SupermarketCheckout/SupermarketCheckout/Checkout.cs
+++ SupermarketCheckout/SupermarketCheckout/Checkout.cs
@@ -1,32 +1,48 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using SupermarketCheckout.Entities;
 using SupermarketCheckout.Utils;
 
 namespace SupermarketCheckout
 {
+    /// <summary>
+    ///     Class which represents a checkout where items can be added and paid.
+    /// </summary>
     public class Checkout
     {
-        public DiscountCollection DiscountCollection { get; set; }
-        private Dictionary<Item, int> items = new Dictionary<Item, int>();
-        private static readonly Discount NoDiscount = new Discount {Name = "NoDiscount", Price = 0, Quantity = -1};
+        /// <summary>
+        ///     Constant which represents a not available discount.
+        /// </summary>
+        public static readonly Discount NoDiscount = new Discount {Name = "NoDiscount", Price = 0, Quantity = -1};
 
+        private readonly Dictionary<Item, int> items = new Dictionary<Item, int>();
+
+        /// <summary>
+        ///     The <see cref="SupermarketCheckout.DiscountCollection" /> for setup discounts in a specific <see cref="DateTime" />
+        ///     interval.
+        /// </summary>
+        public DiscountCollection DiscountCollection { get; set; }
+
+        /// <summary>
+        ///     Add items to checkout.
+        /// </summary>
+        /// <param name="amount">The item amount.</param>
+        /// <param name="item">The item for checkout.</param>
         public void AddItem(int amount, Item item)
         {
             Checks.CheckArgumentNotNull(item, "Item can't be null.");
             Checks.CheckArgument(amount > 0, "Amount must be > 0.");
 
             if (items.TryGetValue(item, out var currentAmount))
-            {
                 items[item] = currentAmount + amount;
-            }
             else
-            {
                 items.Add(item, amount);
-            }
         }
 
+        /// <summary>
+        ///     Pay the added items (with discounts) (<see cref="AddItem" />) and get a <see cref="CheckoutBill" />.
+        /// </summary>
+        /// <returns>The <see cref="CheckoutBill" />.</returns>
         public CheckoutBill PayItems()
         {
             var checkoutBill = new CheckoutBill();
@@ -48,10 +64,7 @@ namespace SupermarketCheckout
 
         private Discount GetDiscount(Item item)
         {
-            if (DiscountCollection.IsValid(DateTime.Now))
-            {
-                return DiscountCollection.GetOrDefault(item, NoDiscount);
-            }
+            if (DiscountCollection.IsValid(DateTime.Now)) return DiscountCollection.GetOrDefault(item, NoDiscount);
 
             //Log discount is not valid.
             Console.WriteLine($"DiscountCollection is invalid with date range {DiscountCollection.ValidityRange}.");
