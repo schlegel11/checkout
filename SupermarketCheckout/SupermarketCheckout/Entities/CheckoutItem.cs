ebb84468261b1d9d16864dde09e5888bc783bffd
Marcel Schlegel <marcel@schlegel11.de>
2019-06-13 11:18:43 +0200

    Add Equatable.

--- SupermarketCheckout/SupermarketCheckout/Entities/CheckoutItem.cs
+++ SupermarketCheckout/SupermarketCheckout/Entities/CheckoutItem.cs
@@ -1,14 +1,54 @@
+using System;
 using System.Text;
 
 namespace SupermarketCheckout.Entities
 {
-    public class CheckoutItem
+    public class CheckoutItem : IEquatable<CheckoutItem>
     {
         public Item Item { get; set; }
         public Discount Discount { get; set; }
         public int Amount { get; set; }
         public int AppliedDiscounts { get; set; }
         public decimal Price { get; set; }
+
+        public bool Equals(CheckoutItem other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(Item, other.Item) && Equals(Discount, other.Discount) && Amount == other.Amount &&
+                   AppliedDiscounts == other.AppliedDiscounts && Price == other.Price;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((CheckoutItem) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Item != null ? Item.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Discount != null ? Discount.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Amount;
+                hashCode = (hashCode * 397) ^ AppliedDiscounts;
+                hashCode = (hashCode * 397) ^ Price.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(CheckoutItem left, CheckoutItem right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(CheckoutItem left, CheckoutItem right)
+        {
+            return !Equals(left, right);
+        }
 
         public override string ToString()
         {
